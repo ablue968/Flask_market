5761d081247eac30cec4c14e70600b2744bab52b
Jhon Pereira <ablue968@gmail.com>
2021-06-20 14:44:04 +0200

    learing to modify the db

--- market.py
+++ market.py
@@ -7,11 +7,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 db=SQLAlchemy(app)
 
 class Item(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(length=30), nullable=False, unique=True)
-    price = db.Column(db.Integer(), nullable=False)
-    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
-    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    ID = db.Column(db.Integer(), primary_key=True)
+    Name = db.Column(db.String(length=30), nullable=False, unique=True)
+    Price = db.Column(db.Integer(), nullable=False)
+    Director = db.Column(db.String(length=12), nullable=False, unique=True)
+    Description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+
+    def __repr__(self):
+        return f'Item {self.Name}'
 
 
 
