621919e081bc1fd8af6fbd63cbf60900ecf5c9f4
Jhon Pereira <ablue968@gmail.com>
2021-06-21 19:27:19 +0200

    user authentification done

--- market/routes.py
+++ market/routes.py
@@ -1,8 +1,11 @@
 from market import app
 from flask import render_template, redirect, url_for, flash
 from market.models import Movie, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
 from market import db
+from flask_login import login_user
+
+
 @app.route('/')
 @app.route('/home')
 def home_page():
@@ -33,3 +36,18 @@ def register_page():
 
     return render_template('register.html', form = form)
 
+@app.route('/login', methods=['GET','POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+
+        attempted_user = User.query.filter_by(username = form.username.data).first()
+
+        if attempted_user and attempted_user.check_password_correction(attempted_password = form.password.data):
+            login_user(attempted_user)
+            flash(f'Success! Welcome back {attempted_user.username}', category="success")
+            return redirect(url_for('market_page'))
+        
+        flash('Username and password are not match! Please try again.', category="danger")
+
+    return render_template('login.html', form = form)
