793bc00ade74742d8e3481dd09b0823289061859
Jhon Pereira <ablue968@gmail.com>
2021-06-21 13:13:50 +0200

    flashes and validations

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,21 @@
 from flask_wtf import FlaskForm
 from flask_wtf.recaptcha import validators
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
-
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from market.models import User
 
 class RegisterForm(FlaskForm):
+
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already taken, please try a different one')
+
+    def validate_email(self, email_to_check):
+        email = User.query.filter_by(email=email_to_check.data).first()
+        if email:
+            raise ValidationError('Please try with another email address, this one already exist.')
+
     username= StringField(label="User Name:", validators=[Length(min=4,max=30), DataRequired()])
     email = StringField(label="Email Address:", validators=[Email(), DataRequired()])
     password1 = PasswordField(label="Password:", validators=[Length(min=6), DataRequired()])
--- market/routes.py
+++ market/routes.py
@@ -1,5 +1,5 @@
 from market import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from market.models import Movie, User
 from market.forms import RegisterForm
 from market import db
@@ -29,7 +29,7 @@ def register_page():
 
     if form.errors != {}: #If there are not errors from the validations
         for err_msg in form.errors.values():
-            print(f' Ups! There seems to be an error creating the user: {err_msg}')
+            flash(f' Ups! There seems to be an error creating the user: {err_msg}', category='danger')
 
     return render_template('register.html', form = form)
 
